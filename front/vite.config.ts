01373a448aa0146d3eab950191d6ab7705b25f6f
Klayton leandro <49959858+klayton-leandro@users.noreply.github.com>
2023-09-13 13:03:10 -0300

    chore(main): ajustando importações com o vite e configurações

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: 'styles', replacement: '/src/styles' },
+      { find: 'contexts', replacement: '/src/contexts' },
+      { find: 'components', replacement: '/src/components' },
+      { find: 'pages', replacement: '/src/pages' },
+      { find: 'routes', replacement: '/src/routes' },
+      { find: 'services', replacement: '/src/services' },
+      { find: 'utils', replacement: '/src/utils' },
+      { find: 'assets', replacement: '/src/assets' },
+      { find: 'consts', replacement: '/src/consts' },
+      { find: 'envs', replacement: '/src/envs' },
+      { find: 'store', replacement: '/src/store' },
+    ],
+   }
 })
