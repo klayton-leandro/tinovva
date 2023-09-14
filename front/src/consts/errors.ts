27fc4a24727f94cd63aae814b857a3ca2ee3dcba
Klayton leandro <49959858+klayton-leandro@users.noreply.github.com>
2023-09-14 12:14:22 -0300

    feat(main): adicionando constantes de erros

---
+++ front/src/consts/errors.ts
@@ -0,0 +1,7 @@
+export const forms = {
+    REQUIRED: "Campo obrigatório.",
+    INVALID_EMAIL: "E-mail inválido.",
+    INVALID_NAME: "Campo deve conter 3 caracteres ou mais",
+    PHONE_FORMAT: "O telefone não está em um formato válido. Formato esperado: (xx) xxxxx-xxxx",
+    INVALID_CPF: 'CPF inválido.',
+}
